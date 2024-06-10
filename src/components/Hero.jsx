17119c48adec642323854a5f95c71964edee81dc
William Koszelnik <william.koszelnik@gmail.com>
2024-06-10 13:02:49 +0200

    Nav + Hero terminé

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import { HERO_CONTENT } from "../constants";
+import profilePic from "../assets/williamKosz.jpg"
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+        <div className="flex flex-wrap">
+            <div className="w-full lg:w-1/2">
+                <div className="flex flex-col items-center lg:items-start">
+                    <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                        William Koszelnik
+                    </h1>
+                        <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                            Développeur Web
+                        </span>
+                        <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-justify">
+                            {HERO_CONTENT}
+                        </p>
+                    </div>
+                </div>
+                <div className="w-full lg:w-1/2 lg:p-8">
+                    <div className="flex justify-center">
+                        <img className="rounded-lg" src={profilePic} alt="Photo de William Koszelnik" />
+                    </div>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Hero
---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/WkLogo.png"
+import { FaLinkedin } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
+
+const Navbar = () => {
+  return (
+    <nav className="mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            <img className="mx-2 w-20" src={logo} alt="logo WK" />
+        </div>
+        <div className="m8 flex items-center justify-center gap-4 text-2xl">
+          <FaLinkedin />
+          <FaGithub />
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
