e915432de8b34a633b2157bbe41cca022bf3999a
William Koszelnik <william.koszelnik@gmail.com>
2024-06-16 20:58:24 +0200

    ajout photo sans fond, et correction padding photo hero

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -25,7 +25,7 @@ const About = () => {
               <motion.div 
               whileInView={{opacity: 1, x: 0}}
               initial={{opacity: 0, x: -100}}
-              transition={{duration: 0.5}}
+              transition={{duration: 0.7}}
               viewport={{ once: true }}
               className="w-full lg:w-1/2 lg:p-8">
 
@@ -37,7 +37,7 @@ const About = () => {
               <motion.div 
               whileInView={{opacity: 1, x: 0}}
               initial={{opacity: 0, x: 100}}
-              transition={{duration: 0.5}}
+              transition={{duration: 0.7}}
               viewport={{ once: true }}
               className="w-full lg:w-1/2">
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/williamKosz.webp"
+import profilePic from "../assets/photo_william_koszelnik_sans_fond.webp"
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
@@ -41,7 +41,7 @@ const Hero = () => {
                         </motion.p>
                     </div>
                 </div>
-                <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="w-full lg:w-1/2 relative">
                     <div className="flex justify-center">
                         <motion.img 
                         initial={{ x: 100, opacity: 0 }}
